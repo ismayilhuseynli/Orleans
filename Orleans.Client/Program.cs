8e61f2d0af5f3c46903a3173e28a2e62cb4b5cbe
Ismayil Huseynli <huseynli.ismayil@hotmail.com>
2023-02-24 00:04:07 +0400

    create Orleans.Client project

---
+++ Orleans.Client/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Orleans.ClientObservers;
+using Orleans.Configuration;
+using Orleans.GrainInterfaces;
+
+
+
+namespace Orleans.Client;
+
+class Program
+{
+    static async Task Main()
+    {
+
+        using IHost host = Host.CreateDefaultBuilder()
+            .UseOrleansClient((context, client) =>
+            {
+                client.Configure<ClusterOptions>(options =>
+                {
+                    options.ClusterId = "cluster";
+                    options.ServiceId = "MyOrleansService";
+                }).UseLocalhostClustering();
+
+            })
+            .UseConsoleLifetime()
+            .Build();
+
+
+        await host.StartAsync();
+
+        IGrainFactory client = host.Services.GetRequiredService<IGrainFactory>();
+
+        while (true)
+        {
+            Console.WriteLine("Please enter a robot name...");
+            var grainId = Console.ReadLine();
+            var grain = client.GetGrain<IRobotGrain>(grainId);
+            Console.WriteLine("Please enter an instruction...");
+            var instruction = Console.ReadLine();
+            await grain.AddInstruction(instruction);
+            var count = await grain.GetInstructionCount();
+            Console.WriteLine($"{grainId} has {count} instruction(s)");
+        }
+
+    }
+}
+
