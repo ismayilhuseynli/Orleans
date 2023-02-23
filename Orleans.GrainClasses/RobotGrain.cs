6c8c2e4c93b406135b10511c76b0e4592e576853
Ismayil Huseynli <huseynli.ismayil@hotmail.com>
2023-02-24 00:03:35 +0400

    create Orleans.GrainClasses project

---
+++ Orleans.GrainClasses/RobotGrain.cs
@@ -0,0 +1,105 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Orleans;
+using Orleans.GrainInterfaces;
+using Orleans.Runtime;
+using Orleans.Streams;
+
+namespace Orleans.GrainClasses;
+
+public class RobotGrain : Grain, IRobotGrain
+{
+    private ILogger<RobotGrain> _logger;
+    IPersistentState<RobotState> _state;
+    string _key;
+    IAsyncStream<InstructionMessage> _stream;
+
+    public RobotGrain(ILogger<RobotGrain> logger, [PersistentState("robotState", "robotStateStore")]
+        IPersistentState<RobotState> state)
+    {
+        _logger = logger;
+        _state = state;
+        _key = this.GetPrimaryKeyString();
+
+        _stream = this
+            .GetStreamProvider("SMSProvider")
+            .GetStream<InstructionMessage>(
+                Guid.Empty);
+    }
+    public async Task AddInstruction(string instruction)
+    {
+
+        _logger.LogWarning($"{_key} adding '{instruction}'");
+
+        _state.State.Instructions.Enqueue(instruction);
+        await _state.WriteStateAsync();
+    }
+    public Task<int> GetInstructionCount()
+    {
+        return Task.FromResult(_state.State.Instructions.Count);
+    }
+    public async Task<string> GetNextInstruction()
+    {
+        if (_state.State.Instructions.Count == 0)
+        {
+            return null;
+        }
+        var instruction = _state.State.Instructions.Dequeue();
+
+        _logger.LogWarning($"{_key} returning '{instruction}'");
+
+        await this.Publish(instruction);
+
+        await _state.WriteStateAsync();
+        return instruction;
+    }
+
+    private Task Publish(string instruction)
+    {
+        var message = new InstructionMessage(
+            instruction, _key);
+        return _stream.OnNextAsync(message);
+    }
+}
+
+
+#region version 1
+//public class RobotGrain : Grain, IRobotGrain
+//{
+//    private ILogger<RobotGrain> _logger;
+//    private Queue<string> instructions = new Queue<string>();
+
+
+//    public RobotGrain(ILogger<RobotGrain> logger)
+//    {
+//        _logger = logger;
+//    }
+//    public Task AddInstruction(string instruction)
+//    {
+//        var key = this.GetPrimaryKeyString();
+//        _logger.LogWarning($"{key} adding '{instruction}'");
+//        this.instructions.Enqueue(instruction);
+//        return Task.CompletedTask;
+//    }
+//    public Task<int> GetInstructionCount()
+//    {
+//        return Task.FromResult(this.instructions.Count);
+//    }
+//    public Task<string> GetNextInstruction()
+//    {
+//        if (this.instructions.Count == 0)
+//        {
+//            return Task.FromResult<string>(null);
+//        }
+//        var instruction = this.instructions.Dequeue();
+
+//        var key = this.GetPrimaryKeyString();
+//        _logger.LogWarning($"{key} adding '{instruction}'");
+
+//        return Task.FromResult(instruction);
+//    }
+//}
+
+
+#endregion
+
---
+++ Orleans.GrainClasses/SubscriberGrain.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Orleans.GrainInterfaces;
+using Orleans.Streams;
+
+namespace Orleans.GrainClasses;
+
+[ImplicitStreamSubscription("StartingInstruction")]
+public class SubscriberGrain : Grain, ISubscriberGrain, IAsyncObserver<InstructionMessage>
+{
+    public override async Task OnActivateAsync(CancellationToken token)
+    {
+        var key = this.GetPrimaryKey();
+
+        await this.GetStreamProvider("SMSProvider")
+            .GetStream<InstructionMessage>(key)
+            .SubscribeAsync(this);
+
+        await base.OnActivateAsync(token);
+    }
+    public Task OnNextAsync(
+        InstructionMessage instruction,
+        StreamSequenceToken token = null)
+    {
+        var msg = $"{instruction.Robot} starting {instruction.Instruction}";
+        Console.WriteLine(msg);
+        return Task.CompletedTask;
+    }
+    public Task OnCompletedAsync() =>
+        Task.CompletedTask;
+
+    public Task OnErrorAsync(System.Exception ex) =>
+        Task.CompletedTask;
+}
+
+
